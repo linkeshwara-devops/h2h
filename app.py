e02cf2fb7ca7dcbc779d727e98f9db314d138f39
Linkeshwara N S <linkeshwara.off@gmail.com>
2026-04-20 08:29:55 +0530

    Change home route to render index.html
    Updated home route to render index.html instead of returning a string. Removed debug mode from app.run.

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello World"
+    return render_template("index.html")   # loads your website
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
-    @app.route("/")
-def home():
-    return "Hello World"
+    app.run(host="0.0.0.0", port=5000)
